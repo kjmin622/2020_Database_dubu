d441736654ab2da85615ac8505c9d076ff2f3962
kjmin622 <kjmin622@naver.com>
2020-11-16 02:03:50 +0900

    add : mainpage

--- src/dubu/page/urls.py
+++ src/dubu/page/urls.py
@@ -5,6 +5,7 @@ from . import admin_views
 urlpatterns = [
     path('',views.index,name="index"),
     path('about',views.about,name="about"),
+    path('rooms',views.rooms,name="rooms"),
     path('admin',admin_views.admin,name="admin"),
     path('admin/login',admin_views.adminSignin,name="admin_login"),
 ]
