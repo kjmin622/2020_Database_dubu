c8f07937f2fbfd589961b303ce480b06e8de37c6
iopopoi <kimkin0668@gmal.com>
2020-11-16 18:51:24 +0900

    fix: menu기능을 위한 수정

--- src/dubu/page/urls.py
+++ src/dubu/page/urls.py
@@ -1,7 +1,36 @@
+"""dubu URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.urls import path
 from . import views
+from . import admin_views
 
 urlpatterns = [
+    #main
     path('',views.index,name="index"),
-    path('admin',views.admin,name="admin")
+    path('about',views.about,name="about"),
+    path('rooms',views.rooms,name="rooms"),
+    path('reservation',views.reservation,name="reservation"),
+    path('join',views.join,name="join"),
+    path('event',views.event,name="event"),
+    path('login',views.login,name="login"),
+    path('mypage',views.mypage,name="mypage"),
+
+    #admin
+    path('admin',admin_views.admin,name="admin"),
+    path('admin/login',admin_views.adminLogin,name="admin_login"),
+    path('admin/logout',admin_views.adminLogout,name="admin_logout"),
+    path('admin/manage_depart',admin_views.manage_depart,name="manage_depart"),
+
 ]
--- src/dubu/page/views.py
+++ src/dubu/page/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.db import connection
+from .models import *
+from .forms import *
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, this is MainPage")
+    return render(request,'main/index.html',{})
 
-def admin(request):
-    return HttpResponse("Hello, this is adminPage")
+def about(request):
+    return render(request,'main/about.html',{})
+
+def rooms(request):
+    return render(request,'main/rooms-single.html',{})
+
+def reservation(request):
+    return render(request,'main/reservation.html',{})
+
+def join(request):
+    return render(request,'main/join.html',{})
+
+def event(request):
+    return render(request,'main/event.html',{})
+
+def login(request):
+    return render(request,'main/login.html',{})
+
+def mypage(request):
+    return render(request,'main/mypage.html',{})
