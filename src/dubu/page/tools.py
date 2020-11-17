a3f4e2849a5620e47edb5231557221e5e31be64f
kjmin622 <kjmin622@naver.com>
2020-11-17 09:17:14 +0900

    fix: bugfix

--- src/dubu/page/tools.py
+++ src/dubu/page/tools.py
@@ -7,7 +7,7 @@ class staff():
         try:
             if(dataDir=={}):
                 cursor = connection.cursor()
-                sqlStr = "select staff_id,first_name,last_name,rank,depart_id,status,bank,account,phone,wide_area_unit,basic_unit,street,si_gu,eub_myeon,building_number,detail_address,team_name from page_staff  natural join (select * from page_team_staff natural join (select page_staff_info.staff_id,first_name,last_name,bank,account,phone,wide_area_unit,basic_unit,street,si_gu,eub_myeon,building_number,detail_address from page_staff_address inner join page_staff_info))"
+                sqlStr = "select staff_id, first_name, last_name, rank,depart_id, status,bank,account, phone,wide_area_unit,basic_unit,street,si_gu,eub_myeon, building_number, detail_address, team_name from page_staff  natural join (select * from page_team_staff natural join (select page_staff_info.staff_id, first_name, last_name, bank,account,phone,wide_area_unit,basic_unit,street,si_gu,eub_myeon,building_number,detail_address from page_staff_address inner join page_staff_info on page_staff_info.staff_id = page_staff_address.staff_id))"
                 result = cursor.execute(sqlStr)
                 datas = cursor.fetchall()
                 connection.close()
@@ -17,7 +17,6 @@ class staff():
                     output_data.append({'staff_id':data[0], 'first_name':data[1], 'last_name':data[2], 'rank':data[3],
                                         'depart_id':data[4], 'status':data[5], 'bank':data[6], 'account':data[7],
                                         'phone':data[8],'wide_area_unit':data[9],'basic_unit':data[10],'street':data[11],'si_gu':data[12],'eub_myeon':data[13],'buildding_number':data[14],'detail_address':data[15], 'team':data[16]})
-
 
                 return output_data
 
@@ -75,10 +74,10 @@ class staff():
     def edit_staff(dataDir):
         try:
             staff_id = dataDir["staff_id"];team=dataDir["team"];rank = dataDir["rank"];status = dataDir["status"];depart_id = dataDir["depart_id"];first_name = dataDir["first_name"];last_name = dataDir["last_name"];phone = dataDir["phone"];bank = dataDir["bank"];account = dataDir["account"];wide_area_unit = dataDir["wide_area_unit"];street = dataDir["street"];basic_unit = dataDir["basic_unit"];si_gu = dataDir["si_gu"];eub_myeon = dataDir["eub_myeon"];building_number = dataDir["building_number"];detail_address = dataDir["detail_address"]
-            sqlStr1 = f"update page_staff set rank='{rank}',status='{status}',depart_id='{depart_id}' where staff_id={staff_id}"
-            sqlStr2 = f"update page_staff_info set first_name='{first_name}',last_name='{last_name}',phone='{phone}',bank='{bank}',account='{account}') where staff_id={staff_id}"
-            sqlStr3 = f"update page_staff_address(wide_area_unit='{wide_area_unit}',street='{street}',basic_unit='{basic_unit}',si_gu='{si_gu}',eub_myeon='{eub_myeon}',building_number='{building_number}',detail_address='{detail_address}') where staff_id={staff_id}"
-            sqlStr4 = f"update page_team_staff set team_name='{team}' where staff_id={staff_id}"
+            sqlStr1 = f"update page_staff set rank='{rank}',status='{status}',depart_id='{depart_id}' where staff_id='{staff_id}'"
+            sqlStr2 = f"update page_staff_info set first_name='{first_name}',last_name='{last_name}',phone='{phone}',bank='{bank}',account='{account}' where staff_id='{staff_id}'"
+            sqlStr3 = f"update page_staff_address set wide_area_unit='{wide_area_unit}',street='{street}',basic_unit='{basic_unit}',si_gu='{si_gu}',eub_myeon='{eub_myeon}',building_number='{building_number}',detail_address='{detail_address}' where staff_id='{staff_id}'"
+            sqlStr4 = f"update page_team_staff set team_name='{team}' where staff_id='{staff_id}'"
             cursor = connection.cursor()
             cursor.execute(sqlStr1);cursor.fetchall()
             cursor.execute(sqlStr2);cursor.fetchall()
