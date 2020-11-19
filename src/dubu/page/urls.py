816c5e0e1157a3873c22aef116c1f4c5e9eba1de
huskycat1202 <annie991202@naver.com>
2020-11-19 19:12:06 +0900

    add :merge

--- src/dubu/page/urls.py
+++ src/dubu/page/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('about',views.about,name="about"),
     path('rooms',views.rooms,name="rooms"),
+    path('reservation',views.reservation,name="reservation"),
+    path('join',views.join,name="join"),
+    path('event',views.event,name="event"),
     path('login',views.login,name="login"),
+    path('mypage',views.mypage,name="mypage"),
     path('signup',views.signup,name="signup"),
 
     #admin
