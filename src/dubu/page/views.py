3535f9391916a27115a91eb976a6cd4cda37fda8
huskycat1202 <annie991202@naver.com>
2020-11-24 16:00:24 +0900

    fix:again

--- src/dubu/page/views.py
+++ src/dubu/page/views.py
@@ -42,11 +42,11 @@ def reservation(request):
     # login
     else:
        login = True
-    #reserve
+    # #reserve
     if request.method == "POST":
         try:
             check_in = request.POST['check_in'], check_out = request.POST['check_out'], adult_num = request.POST['adult_num'], child_num= request.POST['child_num'], baby_num= request.POST['baby_num']
-            if(request.POST['check_in']=='' or request.POST['check_out']=='' or check_in>=check_out or datetime.today()>=check_in):
+            if(request.POST['check_in']=='' or request.POST['check_out']=='' or check_in>=check_out or date.today().strftime("%Y-%m-%d")>=check_in):
                 print(check_in)
                 return render(request,'main/reservation.html',{'Error': 'Check the date again.'})
             if(adult_num<=0 or (adult_num+child_num+baby_num)==0 or (adult_num+child_num+baby_num)>=4):
@@ -54,7 +54,7 @@ def reservation(request):
         except:
             return render(request,'main/reservation.html',{})
         return render(request,'main/reservation2.html',{})
-    return render(request,'main/reservation.html',{"login":login})
+    return render(request,'main/reservation.html',{})
 
 def reservation2(request):
     # not login
@@ -63,7 +63,16 @@ def reservation2(request):
     # login
     else:
         login = True
-    return render(request,'main/reservation2.html',{"login":login})
+    
+    if(request.method=='POST'):
+        if(request.POST['method']=='reservation'):
+            check_in = request.POST['check_in'];check_out = request.POST['check_out'];adult_num = request.POST['adult_num'];child_num= request.POST['child_num'];baby_num= request.POST['baby_num']
+            if(request.POST['check_in']=='' or request.POST['check_out']=='' or check_in>=check_out or date.today().strftime("%Y-%m-%d")>=check_in):
+                return redirect('reservation')
+            if(adult_num<=0 or (adult_num+child_num+baby_num)==0 or (adult_num+child_num+baby_num)>=4):
+                return redirect('reservation')
+            return render(request,'main/reservation2.html',{"check_in":check_in, "check_out":check_out, "adult_num":adult_num, "child_num": child_num, "baby_num": baby_num})
+    return redirect('reservtion')
 
 def reservation3(request):
     # not login
